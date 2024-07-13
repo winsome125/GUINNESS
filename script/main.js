ad3af8e3012f752daa8527e621495921c75a1eef
winsome125 <134434705+winsome125@users.noreply.github.com>
2024-07-13 17:58:37 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+
+  $('.hi_con02').show();
+
+
+  $('#b01').click(function(){
+    $('.hi_c').hide();
+    $('.hi_con02').show();
+    return false;
+  });
+
+  $('#b02').click(function(){
+    $('.hi_c').hide();
+    $('.hi_con03').show();
+    return false;
+  });
+
+  $('#b03').click(function(){
+    $('.hi_c').hide();
+    $('.hi_con04').show();
+    return false;
+  });
+
+  $('#b04').click(function(){
+    $('.hi_c').hide();
+    $('.hi_con05').show();
+    return false;
+  });
+
+  $('#b05').click(function(){
+    $('.hi_c').hide();
+    $('.hi_con06').show();
+    return false;
+  });
+
+  $('#b06').click(function(){
+    $('.hi_c').hide();
+    $('.hi_con07').show();
+    return false;
+  });
+
+
+});
